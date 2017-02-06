d59494a89ba371cc394f9a5a05e6cebaf35b9c01
TusharSomwanshi <somwanshi.tushar@gmail.com>
2017-02-06 11:40:30 +0530

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
-app.listen(3000,function()
-{
-  console.log('Express server is up on port 3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
